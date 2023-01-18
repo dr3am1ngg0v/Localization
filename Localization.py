17ba4775b9e8f68b89e7b10d33aae74cded574b5
Johannes Mitrofanov <workmail@dr3am1ngg.ru>
2023-01-18 21:43:31 +0500

    Reworked original code

--- Localization.py
+++ Localization.py
@@ -1,45 +1,83 @@
-class Localization:
-    def __init__(self, language):
-        self.language = language
-        self.translations = {}
+import os
 
-    def add_translations(self, translations):
-        self.translations.update(translations)
-
-    def get_translation(self, key):
-        return self.translations.get(key, key)
+os.system('cls')
 
 class Robot:
-    def __init__(self, name, inventory_number, company_name, localization):
-        self.name = name
-        self.inventory_number = inventory_number
-        self.functions = []
-        self.company_name = company_name
-        self.localization = localization
+    __instance = None
 
-    def add_function(self, function):
-        self.functions.append(function)
+    def __new__(cls):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+            cls.__instance.name = "V"
+            cls.__instance.inventory_number = "AA001221-56"
+            cls.__instance.operating_company = None
+            cls.__instance.functions = []
+            cls.__instance.language = "English"
+        return cls.__instance
 
-    def list_functions(self):
-        return [self.localization.get_translation(func) for func in self.functions]
-
-    def change_name(self, new_name):
+    def set_name(self, new_name):
         self.name = new_name
 
-localization_en = Localization("en")
-localization_en.add_translations({"build_house": "Build House", "build_shed": "Build Shed", "add_floor": "Add Floor", "remove_top_floor": "Remove Top Floor"})
+    def set_operating_company(self, new_operating_company):
+        self.operating_company = new_operating_company
 
-localization_ru = Localization("ru")
-localization_ru.add_translations({"build_house": "Построить Дом", "build_shed": "Построить Сарай", "add_floor": "Добавить Этаж", "remove_top_floor": "Удалить Верхний Этаж"})
+    def set_language(self, new_language):
+        self.language = new_language
 
-robot = Robot("V", "AA001221-56", "Primary_school", localization_en)
-robot.add_function("build_house")
-robot.add_function("build_shed")
+    def get_info(self):
+        if self.language == "English":
+            return f"Name: {self.name}, Inventory Number: {self.inventory_number}, Operating Company: {self.operating_company}, Functions: {self.functions}"
+        elif self.language == "Russian":
+            return f"Имя: {self.name}, Инвентарный номер: {self.inventory_number}, Операционная компания: {self.operating_company}, Функции: {self.functions}"
+        else:
+            return f"Invalid Language. Try another."
 
-robot.change_name("VITA")
-robot.company_name = "OOO Koshmarik"
-robot.add_function("add_floor")
-robot.add_function("remove_top_floor")
+class RobotDecorator:
+    def __init__(self, robot):
+        self.robot = robot
 
-robot.localization = localization_ru
-print(robot.list_functions()) # prints ['Построить Дом', 'Построить Сарай', 'Добавить Этаж', 'Удалить Верхний Этаж']
+    def build_house(self):
+        self.robot.functions.append("build_house")
+        if self.robot.language == "Russian":
+            self.robot.functions[-1] = "построить дом"
+
+    def build_shed(self):
+        self.robot.functions.append("build_shed")
+        if self.robot.language == "Russian":
+            self.robot.functions[-1] = "построить сарай"
+
+    def add_floor(self):
+        self.robot.functions.append("add_floor")
+        if self.robot.language == "Russian":
+            self.robot.functions[-1] = "добавить этаж"
+
+    def remove_floor(self):
+        self.robot.functions.append("remove_floor")
+        if self.robot.language == "Russian":
+            self.robot.functions[-1] = "снести этаж"
+
+if __name__ == "__main__":
+    robot = Robot()
+    robot_decorator = RobotDecorator(robot)
+    print("1.", robot.get_info())
+    # Name: V, Inventory Number: AA001221-56, Operating Company: None, Functions: []
+
+    robot_decorator.build_house()
+    robot_decorator.build_shed()
+    print("2.", robot.get_info())
+    # Name: V, Inventory Number: AA001221-56, Operating Company: None, Functions: ['build_house', 'build_shed']
+
+    robot.set_name("VITA")
+    robot.set_operating_company("OOO Koshmarik")
+    print("3.", robot.get_info())
+    # Name: VITA, Inventory Number: AA001221-56, Operating Company: OOO Koshmarik, Functions: ['build_house', 'build_shed']
+
+    robot_decorator.add_floor()
+    robot_decorator.remove_floor()
+    print("4.", robot.get_info())
+    # Name: VITA, Inventory Number: AA001221-56, Operating Company: OOO Koshmarik, Functions: ['build_house', 'build_shed', 'add_floor', 'remove_floor']
+
+    robot.set_name("Vitaliy")
+    robot.set_operating_company("OOO Koshmarik")
+    print("5.", robot.get_info())
+    # Name: Vitaliy, Inventory Number: AA001221-56, Operating Company: OOO Koshmarik, Functions: ['build_house', 'build_shed', 'add_floor', 'remove_floor']
